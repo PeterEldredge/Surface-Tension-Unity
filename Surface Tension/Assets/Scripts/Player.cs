57f251f3b76b3f3541ad35ff06f064d9994c788c
Drew Graham <Drew Graham>
2018-05-13 20:31:23 -0400

    created player move script

---
+++ Surface Tension/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour 
+{
+	/// <summary>
+	/// Rate at which player moves
+	/// </summary>
+	public float moveSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float horizontal = Input.GetAxis("Horizontal");
+		// float vertical = Input.GetAxis("vertical");
+
+		// Move actor in direction of input
+        Vector3 movement = new Vector2 (horizontal * moveSpeed, 0);
+        gameObject.transform.position += movement;
+	}
+}
