99955115cb8b76b32de3e5937cc9618c77671634
Drew Graham <Drew Graham>
2018-05-13 21:57:59 -0400

    messed with player movement code, made materials

--- Surface Tension/Assets/Scripts/Player.cs
+++ Surface Tension/Assets/Scripts/Player.cs
@@ -9,21 +9,32 @@ public class Player : MonoBehaviour
 	/// Rate at which player moves
 	/// </summary>
 	public float moveSpeed;
+	public float startMoveSpeed;
+	public float startMoveThresh;
+	public float slowMultiplyer;
 
 
-	// Use this for initialization
-	void Start () {
-
-	}
-	
 	// Update is called once per frame
 	void Update () 
 	{
+		Vector2 movement;
+		float velocity = GetComponent<Rigidbody2D>().velocity.x;
 		float horizontal = Input.GetAxis("Horizontal");
 		// float vertical = Input.GetAxis("vertical");
 
 		// Move actor in direction of input
-        Vector3 movement = new Vector2 (horizontal * moveSpeed, 0);
-        gameObject.transform.position += movement;
+		if (Mathf.Abs(velocity) < startMoveThresh){
+			Debug.Log("under startMove threshold");
+			movement = new Vector2 (horizontal * startMoveSpeed * Time.deltaTime, 0);
+		} else {
+			Debug.Log("surpassed startMove threshold");
+        	movement = new Vector2 (horizontal * moveSpeed * Time.deltaTime, 0);
+		}
+
+		if (horizontal == 0 && velocity > 0){
+			movement = new Vector2 (velocity * slowMultiplyer * Time.deltaTime, 0);
+		}
+
+        GetComponent<Rigidbody2D>().velocity += movement;
 	}
 }
