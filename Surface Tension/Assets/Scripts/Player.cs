dede45a6870873e536956fd6c28fe11e29adaba7
DESKTOP-0RN3DGF\Peter Eldredge <pte25@drexel.edu>
2018-05-14 21:33:47 -0400

    Basic Player Controller
    Definetly not perfect, but gives us a place to start. I tried to be very specific as likely all of us will be using this.

--- Surface Tension/Assets/Scripts/Player.cs
+++ Surface Tension/Assets/Scripts/Player.cs
@@ -5,36 +5,67 @@ using UnityEngine;
 
 public class Player : MonoBehaviour 
 {
-	/// <summary>
-	/// Rate at which player moves
-	/// </summary>
-	public float moveSpeed;
-	public float startMoveSpeed;
-	public float startMoveThresh;
-	public float slowMultiplyer;
+    //Distance the player moves each frame
+    public float moveSpeed;
 
+    //Velocity applied to player on jump
+    public float jumpVelocity;
 
-	// Update is called once per frame
-	void Update () 
+    //Affects the gravity of the player as they fall
+    public float fallMultiplyer;
+
+    //Affects the gravity of the player as they jump
+    public float jumpMultiplyer;
+
+    //Initializes pBody, this will be the player's Rigidbody2D component 
+    Rigidbody2D pBody;
+
+    //Is the player on the ground
+    bool isGrounded = true;
+
+    void Awake()
+    {
+        pBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update () 
 	{
-		Vector2 movement;
-		float velocity = GetComponent<Rigidbody2D>().velocity.x;
-		float horizontal = Input.GetAxis("Horizontal");
-		// float vertical = Input.GetAxis("vertical");
+        //Basic movement
+        Vector2 movement = new Vector2(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0);
+        transform.Translate(movement);
 
-		// Move actor in direction of input
-		if (Mathf.Abs(velocity) < startMoveThresh){
-			Debug.Log("under startMove threshold");
-			movement = new Vector2 (horizontal * startMoveSpeed * Time.deltaTime, 0);
-		} else {
-			Debug.Log("surpassed startMove threshold");
-        	movement = new Vector2 (horizontal * moveSpeed * Time.deltaTime, 0);
-		}
+        //Jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            pBody.velocity = Vector2.up * jumpVelocity;
+        }
 
-		if (horizontal == 0 && velocity > 0){
-			movement = new Vector2 (velocity * slowMultiplyer * Time.deltaTime, 0);
-		}
-
-        GetComponent<Rigidbody2D>().velocity += movement;
+        //Multiplies the affects of gravity on the player given specific instances
+        if (pBody.velocity.y < 0) //If player falling, increase gravity affect
+        {
+            pBody.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplyer) * Time.deltaTime;
+        } else if (pBody.velocity.y > 0 && !Input.GetButton("Jump")) 
+        {
+            pBody.velocity += Vector2.up * Physics2D.gravity.y * (jumpMultiplyer) * Time.deltaTime;
+        }
 	}
+
+    //Player touches ground
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    //Player leaves ground
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
 }
