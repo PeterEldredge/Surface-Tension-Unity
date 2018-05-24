2405ea79470168ffd5ae0ee2636bfd1b1a157387
Jacob Dixey <jmd585@drexel.edu>
2018-05-23 21:26:31 -0400

    SurfaceChange Onclick functionality prototype

---
+++ Surface Tension/Assets/Scripts/SurfaceChange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurfaceChange : MonoBehaviour {
+
+    private enum surface
+    {
+        BOUNCE,
+        SLIP,
+        STICK
+    }
+
+    
+
+    public Color ncol;
+
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+    void OnMouseDown()
+    {
+        GetComponent<Renderer>().material.color = ncol;
+    }
+}
