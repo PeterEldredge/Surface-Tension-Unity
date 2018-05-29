250079929852493bfa754f0f71ed0aa7495ce914
Drew Graham <Drew Graham>
2018-05-29 13:30:19 -0400

    created SlickSurface

---
+++ Surface Tension/Assets/Scripts/Surface/SlickSurface.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlickSurface : SurfaceMaterial {
+
+	/// <summary>
+	/// Start is called on the frame when a script is enabled just before
+	/// any of the Update methods is called the first time.
+	/// </summary>
+	void OnAwake()
+	{
+		// Set top speed
+		topSpeed = GameObject.FindWithTag("GameController").GetComponent<GameController>().topSpeedMapping[GameController.material.SLIP];
+	}
+}
